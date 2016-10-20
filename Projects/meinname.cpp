afdd6d6b02a10dcebbe5551d3e3eaa5a8e59e6a3
Marie Olbrich <Marie Olbrich>
2016-10-20 14:32:15 +0200

    punkt

--- Projects/meinname.cpp
+++ Projects/meinname.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include <string>
+//
+//int main() {
+//	std::cout << "Wie ist dein Name?" << std::endl;
+//	std::string meinname;
+//	std::cin >> meinname;
+//	std::cout << "Hallo " << meinname << ", wie Alt bist du?" << std::endl;
+//	int meinalter;
+//	std::cin >> meinalter;
+//	std::cout << "Wie groß bist du?" << std::endl;
+//	double meinegroesse;
+//	std::cin >> meinegroesse;
+//	system("pause");
+//}
 
 int main() {
-	std::cout << "Wie ist dein Name?" << std::endl;
-	std::string meinname;
-	std::cin >> meinname;
-	std::cout << "Hallo " << meinname << ", wie Alt bist du?" << std::endl;
-	int meinalter;
-	std::cin >> meinalter;
-	std::cout << "Wie groß bist du?" << std::endl;
-	double meinegroesse;
-	std::cin >> meinegroesse;
-	system("pause");
+	int Temperatur;
+	std::cin >> Temperatur;
+	std::cout << Temperatur;
+	System("pause");
 }
